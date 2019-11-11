272e98454b750eb8653eb50e5a32c3da407cc1e6
Eugene <primak90ua@gmail.com>
2019-11-11 21:24:22 +0200

    yes

--- Console/Command/TestCouponCreate.php
+++ Console/Command/TestCouponCreate.php
@@ -3,15 +3,23 @@
 
 namespace Coupon\Target\Console\Command;
 
+use Coupon\Target\Service\GenerateCouponCodesService;
 use Coupon\Target\Setup\SetupService\CreateCartPriceRuleService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Coupon\Target\Service\GenerateCouponService;
 
 
 class TestCouponCreate extends Command
 {
+    private $generateCouponCodes;
+
+    public function __construct(GenerateCouponCodesService $generateCouponCodes, string $name = null)
+    {
+        parent::__construct($name);
+        $this->generateCouponCodes = $generateCouponCodes;
+    }
+
     protected function configure()
     {
         $this->setName('coupon:create')->setDescription('Test coupon Create');
@@ -19,7 +27,9 @@ class TestCouponCreate extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $params = ['length' => 17, 'prefix' => 'HyDABAI-'];
+        $this->generateCouponCodes->execute(2, 8, $params);
         $output->writeln("Done!");
-      
+
     }
 }
