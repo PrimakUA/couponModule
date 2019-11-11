eae620e4426d8811368d9275f9f258bea1cf4f7a
Eugene <primak90ua@gmail.com>
2019-11-11 20:12:56 +0200

    111

---
+++ Service/GenerateCouponCodesService.php
@@ -0,0 +1,43 @@
+<?php
+/* File: app/code/Coupon/Target/Service/GenerateCouponCodesService.php */
+namespace Coupon\Target\Service;
+use Magento\SalesRule\Model\CouponGenerator;
+/**
+ * Class GenerateCouponListService
+ */
+class GenerateCouponCodesService
+{
+    /**
+     * Coupon Generator
+     *
+     * @var CouponGenerator
+     */
+    protected $couponGenerator;
+    /**
+     * GenerateCouponCodesService constructor
+     *
+     * @param CouponGenerator $couponGenerator
+     */
+    public function __construct(CouponGenerator $couponGenerator)
+    {
+        $this->couponGenerator = $couponGenerator;
+    }
+    /**
+     * Generate coupon list for specified cart price rule
+     *
+     * @param int|null $qty
+     * @param int|null $ruleId
+     * @param array $params
+     *
+     * @return void
+     */
+    public function execute(int $qty, int $ruleId, array $params = []): void
+    {
+        if (!$qty || !$ruleId) {
+            return;
+        }
+        $params['rule_id'] = $ruleId;
+        $params['qty'] = $qty;
+        $this->couponGenerator->generateCodes($params);
+    }
+}
--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -1,25 +1,33 @@
 <?php
-
+/* File: app/code/Coupon/Target/Setup/InstallData.php */
 namespace Coupon\Target\Setup;
-
 use Coupon\Target\Setup\SetupService\CreateCartPriceRuleService;
 use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
-
-
+/**
+ * Class InstallData
+ */
 class InstallData implements InstallDataInterface
 {
-
+    /**
+     * Create Cart Price Rule Service
+     *
+     * @var CreateCartPriceRuleService
+     */
     protected $createCartPriceRuleService;
-
-
+    /**
+     * InstallData constructor
+     *
+     * @param CreateCartPriceRuleService $createCartPriceRuleService
+     */
     public function __construct(CreateCartPriceRuleService $createCartPriceRuleService)
     {
         $this->createCartPriceRuleService = $createCartPriceRuleService;
     }
-
-
+    /**
+     * @inheritdoc
+     */
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
         $this->createCartPriceRuleService->execute();
