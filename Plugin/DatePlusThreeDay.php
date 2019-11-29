19a2c5ac81b3cfacc1836ffce539c183520af4e2
Eugene <primak90ua@gmail.com>
2019-11-29 18:24:31 +0200

    red

--- Plugin/DatePlusThreeDay.php
+++ Plugin/DatePlusThreeDay.php
@@ -6,24 +6,20 @@ namespace Coupon\Target\Plugin;
 
 class DatePlusThreeDay
 {
-    public function afterGetToDate(\Magento\SalesRule\Model\Rule $rule)
+    public function afterGetToDate(\Magento\SalesRule\Model\Rule $subject, $result)
     {
-
-        if ($rule->getName() == 'five')
-        {
-            $date = strtotime("+3 day");
-            $dat = date("Y-m-d H:i:s", $date);
-            return $dat;
-        }
-        elseif ($rule->getName() == 'twenty')
-        {
-            $date = strtotime("+1 day");
-            $dat = date("Y-m-d H:i:s", $date);
-            return $dat;
-        }
-        else
-            $ret = $rule->getData();
-            return $ret['to_date'];
+        $result = $subject->getData();
+        if ($subject->getName() == 'five' || $subject->getName() == 'twenty') {
+            if ($subject->getName() == 'five') {
+                $date = strtotime("+3 day");
+                $dat = date("Y-m-d H:i:s", $date);
+                return $dat;
+            } else {
+                $date = strtotime("+1 day");
+                $dat = date("Y-m-d H:i:s", $date);
+                return $dat;
+            }
+        } else
+            return $result['to_date'];
     }
-
 }
