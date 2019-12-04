3355de4053d39eb7a95abbe1ebbf9e3d2fc3a120
Eugene <primak90ua@gmail.com>
2019-12-04 14:38:47 +0200

    finalEmail

--- Controller/Customer/Email.php
+++ Controller/Customer/Email.php
@@ -2,6 +2,7 @@
 
 namespace Coupon\Target\Controller\Customer;
 
+use Coupon\Target\Block\System\Config;
 use Magento\Framework\App\Helper\Context;
 use Magento\Framework\Translate\Inline\StateInterface;
 use Magento\Framework\Escaper;
@@ -13,12 +14,14 @@ class Email extends \Magento\Framework\App\Helper\AbstractHelper
     protected $escaper;
     protected $transportBuilder;
     protected $logger;
+    protected $config;
 
     public function __construct(
         Context $context,
         StateInterface $inlineTranslation,
         Escaper $escaper,
-        TransportBuilder $transportBuilder
+        TransportBuilder $transportBuilder,
+        Config $config
     )
     {
         parent::__construct($context);
@@ -26,11 +29,14 @@ class Email extends \Magento\Framework\App\Helper\AbstractHelper
         $this->escaper = $escaper;
         $this->transportBuilder = $transportBuilder;
         $this->logger = $context->getLogger();
+        $this->config = $config;
     }
 
     public function sendEmail($email, $coupon)
     {
         try {
+            $endDate = $this->sendDate();
+            $persent = $this->config->getValue(2);
             $this->inlineTranslation->suspend();
             $sender = [
                 'name' => $this->escaper->escapeHtml('Magento Store'),
@@ -46,6 +52,8 @@ class Email extends \Magento\Framework\App\Helper\AbstractHelper
                 )
                 ->setTemplateVars([
                     'templateVar' => "$coupon",
+                    'templateVar2' => "$endDate",
+                    'templateVar3' => "$persent",
                     'subject' => 'you have discount code'
                 ])
                 ->setFrom($sender)
@@ -56,5 +64,13 @@ class Email extends \Magento\Framework\App\Helper\AbstractHelper
         } catch (\Exception $e) {
             $this->logger->debug($e->getMessage());
         }
+    }
+
+    public function sendDate()
+    {
+        $tableDate = $this->config->getDate(2);
+        $endDate = strtotime("+$tableDate day");
+        $date = date("Y-m-d H:i:s", $endDate);
+        return $date;
     }
 }
